80f4714f7f8718a42bc5c131ffe2249d85c7bfb6
universe-me <31508316+universe-me@users.noreply.github.com>
2020-03-09 11:44:10 +0800

    Create logo-footer-blue.js

---
+++ logo-footer/js/logo-footer-blue.js
@@ -0,0 +1,1 @@
+document.writeln("<p>服务器及技术支持：<a href=\'https://aaab.online\' target=\'_blank\'><img src=\'https://resources.aaab.online/logo-footer/img/logo-footer-blue.png\' width=\'121\' height=\'24\' style=\'vertical-align: middle\' /></a></p>");
