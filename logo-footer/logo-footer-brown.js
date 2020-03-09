d8d74873f81dfeb56d860e6745a5af154e3bc60a
universe-me <31508316+universe-me@users.noreply.github.com>
2020-03-09 11:31:09 +0800

    Add files via upload

---
+++ logo-footer/logo-footer-brown.js
@@ -0,0 +1,1 @@
+<p>服务器及技术支持：<a href="https://aaab.online" target="_blank"><img src="https://resources.aaab.online/logo-footer/logo-footer-brown.png" width="121" height="24" style="vertical-align: middle" /></a></p>
